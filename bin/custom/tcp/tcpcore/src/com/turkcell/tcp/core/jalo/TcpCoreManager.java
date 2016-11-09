776bddbd20e7d2e027c074aec0de5c8a98249bd8
Oguz Sarigul <oguz.sarigul@enoca.com>
2016-11-09 21:56:50 +0300

    first commit

---
+++ bin/custom/tcp/tcpcore/src/com/turkcell/tcp/core/jalo/TcpCoreManager.java
@@ -0,0 +1,32 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.turkcell.tcp.core.jalo;
+
+import de.hybris.platform.jalo.JaloSession;
+import de.hybris.platform.jalo.extension.ExtensionManager;
+import com.turkcell.tcp.core.constants.TcpCoreConstants;
+import com.turkcell.tcp.core.setup.CoreSystemSetup;
+
+
+/**
+ * Do not use, please use {@link CoreSystemSetup} instead.
+ * 
+ */
+@SuppressWarnings("PMD")
+public class TcpCoreManager extends GeneratedTcpCoreManager
+{
+	public static final TcpCoreManager getInstance()
+	{
+		final ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
+		return (TcpCoreManager) em.getExtension(TcpCoreConstants.EXTENSIONNAME);
+	}
+}
---
+++ bin/custom/tcp/tcpfulfilmentprocess/src/com/turkcell/tcp/fulfilmentprocess/jalo/TcpFulfilmentProcessManager.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.turkcell.tcp.fulfilmentprocess.jalo;
+
+import de.hybris.platform.jalo.JaloSession;
+import de.hybris.platform.jalo.extension.ExtensionManager;
+import com.turkcell.tcp.fulfilmentprocess.constants.TcpFulfilmentProcessConstants;
+
+@SuppressWarnings("PMD")
+public class TcpFulfilmentProcessManager extends GeneratedTcpFulfilmentProcessManager
+{
+	public static final TcpFulfilmentProcessManager getInstance()
+	{
+		ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
+		return (TcpFulfilmentProcessManager) em.getExtension(TcpFulfilmentProcessConstants.EXTENSIONNAME);
+	}
+	
+}
